a5fdfe810f187c01e25cf6317cc9ae15c47341b9
Jayant Patel <96767549+jayantptl@users.noreply.github.com>
2023-07-29 00:12:25 +0530

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -38,4 +38,30 @@ const signupUser = async (req, res) => {
   }
 }
 
-module.exports = { signupUser, loginUser }
+// verify authorization
+const requireAuth = async (req, res) => {
+
+  const { authorization } = req.headers;
+  if (!authorization) {
+      return res.status(401).json({ error: 'Authorization token required' });
+  }
+
+  const token = authorization.split(' ')[1];   // auth header is -> Bearer token
+
+  try {
+      const { _id } = jwt.verify(token, process.env.SECRET); // this gives _id stored in db to that user
+      req.user = await User.findOne({_id}).select('_id');     // we can attach this user _id in req body and use in other routes
+      console.log('Authorization done ' ,req.user);
+      res.status(200).json({token})
+  }
+  catch (error) {
+      console.log(error);
+      res.status(401).json({ error: 'Request is not autthorized' });
+  }
+
+}
+
+
+
+
+module.exports = { signupUser, loginUser, requireAuth }
